6a93c231671bcb3cea1b6fa1c47d693eafc8076a
eddytsai <eddy0921717370@gmail.com>
2018-10-11 15:47:40 +0800

    lab3

---
+++ app/src/main/java/com/example/user/lab3/aty2.java
@@ -0,0 +1,87 @@
+package com.example.user.lab3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class aty2 extends AppCompatActivity {
+    Button sent_bin;
+    String sugar = "無糖";
+    EditText set_drink;
+    String ice_opt = "微冰";
+    Button sent_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aty2);
+
+        RadioGroup rg1 = (RadioGroup)findViewById(R.id.radioGroup);
+        rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId){
+
+                    case R.id.radioButton:
+                        sugar = "無糖";
+                        break;
+                    case R.id.radioButton2:
+                        sugar = "少糖";
+                        break;
+                    case R.id.radioButton3:
+                        sugar = "半糖";
+                        break;
+                    case R.id.radioButton4:
+                        sugar = "糖";
+                        break;
+                }
+            }
+        });
+        RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup2);
+        rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId){
+
+                    case R.id.radioButton5:
+                        sugar = "微冰";
+                        break;
+                    case R.id.radioButton6:
+                        sugar = "少冰";
+                        break;
+                    case R.id.radioButton7:
+                        sugar = "正常冰";
+                        break;
+                }
+            }
+        });
+
+        sent_btn = (Button)findViewById(R.id.button2);
+        sent_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                set_drink = (EditText) findViewById(R.id.editText);
+                String temp = set_drink.getText().toString();
+                Intent i = new Intent();
+                Bundle b = new Bundle();
+                b.putString("sugar_level",sugar);
+                b.putString("drink_level",temp);
+                b.putString("ice_level",ice_opt);
+                i.putExtras(b);
+                setResult(101, i);
+                finish();
+
+            }
+        });
+
+
+
+
+
+
+
+    }
+}
